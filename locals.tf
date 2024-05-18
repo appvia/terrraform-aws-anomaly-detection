d57b30c97dd7df709a26e56680b85869a24a955c
Rohith Jayawardene <gambol99@gmail.com>
2024-05-18 10:13:11 +0100

    feat: enabling the ability to create the monitors and REUSE existing sns and notifications (#5)

--- locals.tf
+++ locals.tf
@@ -4,6 +4,12 @@ locals {
   enable_slack = var.notifications.slack != null ? true : false
   ## Indicates if email notifications are enabled 
   enable_email = var.notifications.email != null ? true : false
+  ## Indicates if we should create the sns topic 
+  enable_sns_topic_creation = var.sns_topic_arn != null ? false : var.enable_sns_topic_creation
+
+  ## The SNS arn we shuld use on the monitor, this is either the one we 
+  ## created or the one provided by the user 
+  sns_topic_arn = var.sns_topic_arn != null ? var.sns_topic_arn : module.notifications[0].sns_topic_arn
 
   ## The configuration for email notifications if enabled 
   email = local.enable_email ? {
